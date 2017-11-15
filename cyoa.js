cf9839bdd6e44226b8de8c5f94395c1ea2ca5727
Sean LeBlanc <sean.s.leblanc@gmail.com>
2017-11-15 00:46:34 -0500

    more string sanitization

--- cyoa.js
+++ cyoa.js
@@ -16,7 +16,11 @@ markov2 = new Markov({
 
 exports = module.exports = function(){
 	var s = (Math.random() < 0.5 ? markov1 : markov2).randomSequence(undefined, Markov.until.bind(undefined, '\0'));
-	
+
+	// remove null + trim
+	s = s.substr(0,s.length-1);
+	s = s.trim();	
+
 	// fix mismatched quotation marks
 	if((s.match(/"/g) || []).length % 2 == 1){
 		if(s.match(/page \d*\.$/)){
@@ -25,6 +29,14 @@ exports = module.exports = function(){
 			s.splice(-1,0,'," ');
 			s = s.join('');
 		}
+	}
+
+	// start with caps
+	s = s.substr(0,1).toUpperCase() + s.substr(1);
+
+	// end with period
+	if(s.substr(-1) != "."){
+		s += ".";
 	}
 
 	return s;
